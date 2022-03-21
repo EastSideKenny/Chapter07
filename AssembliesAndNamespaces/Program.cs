ccb7d1ea5e3b4094fa9982f5b73a6b78f900a0f9
Jerome Jacob <Jerome56jj@hotmail.com>
2022-03-21 21:12:52 +0100

    exercise and chapter closed

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Xml.Linq; //XDocument
 using static System.Console;
 using System; // String
+using Packt.Shared;
+
 
 XDocument doc = new();
 
@@ -8,3 +10,18 @@ string s1 = "Hello";
 String s2 = "World";
 
 WriteLine($"{s1} {s2}");
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine(); // or "00ffc8"
+WriteLine("Is {0} a valid color value? {1}",
+    arg0: hex, arg1: hex.IsValidHex());
+
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine(); // or "<h1 class=\"<\" />"
+WriteLine("Is {0} a valid XML element? {1}",
+    arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+
+Write("Enter a password: ");
+string? password = ReadLine(); // or "secretsauce"
+WriteLine("Is {0} a valid password? {1}",
+    arg0: password, arg1: password.IsValidPassword());
